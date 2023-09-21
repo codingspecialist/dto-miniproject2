4c4d481d8292324767cc78d990d9cf387d450c82
metacoding <getinthere@naver.com>
2023-09-21 14:55:52 +0900

    유효성 검사 완료 - aop

--- src/main/java/com/example/kakao/user/UserRequest.java
+++ src/main/java/com/example/kakao/user/UserRequest.java
@@ -1,5 +1,9 @@
 package com.example.kakao.user;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,8 +14,14 @@ public class UserRequest {
     @Setter
     @ToString
     public static class JoinDTO {
+        @NotEmpty
         private String email;
+
+        @NotEmpty
+        @Size(min = 8, max = 20, message = "8에서 20자 이내여야 합니다.")
         private String password;
+
+        @NotEmpty
         private String username;
 
         public User toEntity() {
@@ -27,7 +37,9 @@ public class UserRequest {
     @Setter
     @ToString
     public static class LoginDTO {
+        @NotEmpty
         private String email;
+        @NotEmpty
         private String password;
     }
 }
--- src/main/java/com/example/kakao/user/UserRestController.java
+++ src/main/java/com/example/kakao/user/UserRestController.java
@@ -1,7 +1,7 @@
 package com.example.kakao.user;
 
 import javax.servlet.http.HttpSession;
-
+import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.kakao._core.utils.ApiUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -22,14 +23,14 @@ public class UserRestController {
 
     // 회원가입
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO requestDTO) {
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors) {
         userService.join(requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO requestDTO) {
+    public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors) {
         User sessionUser = userService.login(requestDTO);
         session.setAttribute("sessionUser", sessionUser);
         return ResponseEntity.ok().body(ApiUtils.success(null));
